6755dbd7e943402081bffca8eeb53921aa8dfb35
xCHZx <chzx94@gmail.com>
2024-04-17 22:56:50 -0500

    Reestrucutra de rutas (Off)

--- routes/api.php
+++ routes/api.php
@@ -11,52 +11,69 @@ use Illuminate\Support\Facades\Route;
 
 
 
-//Public URLs
+//Auth Routes
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
     Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-});
 
-//Private URLs
-Route::middleware(['auth:sanctum'])->group(function () {
-    //Auth
-    Route::prefix('auth')->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout')->middleware('can:auth.logout');
     });
+});
 
-    //Business
-    Route::prefix('business')->group(function () {
-        Route::post('/', [BusinessController::class, 'store'])->name('business.store')->middleware('can:business.store');
-        Route::get('/{id}', [BusinessController::class, 'getById'])->name('business.getById');
-        Route::get('/user/current', [BusinessController::class, 'getAllByCurrentUser'])->name('business.getAllByCurrentUser');
-        Route::put('/user/current/{id}', [BusinessController::class, 'updateByCurrentUser'])->name('business.updateByCurrentUser');
-        Route::get('user/current/visited', [BusinessController::class, 'getVisitedByCurrentUser'])->name('business.getVisitedByCurrentUser');
+//Company Profiles Routes
+Route::prefix('company')->group(function () {
 
+    Route::middleware(['auth:sanctum'])->group(function () {
+
+        //Negocio
+        Route::prefix('business')->group(function () {
+            // Route::post('/', [BusinessController::class, 'storeAsCompany'])->name('business.storeAsCompany'); //Falta validar por company profile
+            // Route::get('/logged-user', [BusinessController::class, 'getAllByCurrentCompany'])->name('business.getAllByCurrentCompany');
+            // Route::put('/{id}/logged-user', [BusinessController::class, 'updateByCurrentCompany'])->name('business.updateByCurrentCompany');
+        });
+
+        //Tarjetas de sellos
+        Route::prefix('stampcard')->group(function () {
+            // Route::post('/', [StampCardController::class, 'storeAsCompany'])->name('stampcard.storeAsCompany');
+            // Route::get('/logged-user', [StampCardController::class, 'getAllByCurrentCompany'])->name('stampcard.getAllByCurrentCompany');
+        });
+
+        //Visitas
+        Route::prefix('visit')->group(function () {
+            // Route::post('/', [VisitController::class, 'storeAsCompany'])->name('visit.storeAsCompany');
+            // Route::get('{id}/logged-user', [StampCardController::class, 'getByIdByCurrentCompany'])->name('stampcard.getByIdByCurrentCompany');
+            // Route::get('/business/{id}', [VisitController::class, 'getAllByBusinessAsCompany'])->name('visit.getAllByBusinessAsCompany');
+            // Route::get('/stampcard/{id}/logged-user', [VisitController::class, 'getAllByStampCardAsCompany'])->name('visit.getAllByStampCardAsCompany');
+        });
     });
 
-    Route::prefix('stampcard')->group(function () {
-        Route::post('/', [StampCardController::class, 'store'])->name('stampcard.store'); //Falta validar por negocio
-        Route::get('/visitor/current', [StampCardController::class, 'getAllByCurrentVisitor'])->name('stampcard.getAllByCurrentVisitor');
-        Route::get('/company/current', [StampCardController::class, 'getAllByCurrentCompany'])->name('stampcard.getAllByCurrentCompany');
-        Route::get('/{id}', [StampCardController::class, 'getById'])->name('stampcard.getById');
-        Route::get('{id}/visitor/current', [StampCardController::class, 'getByIdByCurrentVisitor'])->name('stampcard.getByIdByCurrentVisitor');
 
-    });
-
-    Route::prefix('visit')->group(function () {
-        Route::post('/', [VisitController::class, 'store'])->name('visit.store');
-        Route::get('/stampcard/{id}', [VisitController::class, 'getAllByStampCard'])->name('visit.getAllByStampCard');
-        Route::get('/user/current', [VisitController::class, 'getAllByCurrentUser'])->name('visit.getAllByCurrentUser');
-        Route::get('/business/{id}', [VisitController::class, 'getByBusiness'])->name('visit.getByBusiness');
-    });
-
-    // Route::prefix('reward')->group(function () {
-    //     Route::post('/', [RewardController::class, 'store'])->name('reward.store');
-    //     Route::get('/user/current', [RewardController::class, 'getAllByCurrentUser'])->name('reward.getAllByCurrentUser');
-    //     Route::get('/stampcard/{id}', [RewardController::class, 'getByStampCard'])->name('reward.getByStampCard');
-    //     Route::get('/business/{id}', [RewardController::class, 'getByBusiness'])->name('reward.getByBusiness');
-    // });
 
 });
 
+//Visitor Profiles Routes
+Route::prefix('visitor')->group(function () {
+
+    Route::middleware(['auth:sanctum'])->group(function () {
+        //Negocios
+        Route::prefix('business')->group(function () {
+            // Route::get('/visited/logged-in', [BusinessController::class, 'getVisitedByCurrentVisitor'])->name('business.getVisitedByCurrentVisitor');
+        });
+
+        //Tarjetas de sellos
+        Route::prefix('stampcard')->group(function () {
+            // Route::get('/logged-user', [StampCardController::class, 'getAllByCurrentVisitor'])->name('stampcard.getAllByCurrentVisitor');
+            // Route::get('/{id}', [BusinessController::class, 'getByIdAsVisitor'])->name('business.getByIdAsVisitor');
+        });
+
+        //Visitas
+        Route::prefix('visit')->group(function () {
+            // Route::get('{id}/logged-user', [StampCardController::class, 'getByIdByCurrentVisitor'])->name('stampcard.getByIdByCurrentVisitor');
+            // Route::get('/logged-user', [VisitController::class, 'getAllByCurrentVisitor'])->name('visit.getAllByCurrentVisitor');
+        });
+    });
+
+
+});
 
