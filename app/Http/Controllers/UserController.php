3a34b87a826adec2f85aad956779d629924ac215
rafael payns <rpayns16@gmail.com>
2024-04-20 18:23:05 -0600

    se agrega la edicion de password esta edicion modifica el controlador de usuario y las rutas de la api es una edicion de password solo para usuarios autenticados

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -22,12 +23,12 @@ class UserController extends Controller
         $user->account_status_id = $request->account_status;
         $user->password = hash::make($request->password);
         $repittCode = $this->generateRepittCode();
-        $user->qr_path = asset('storage/business/images/qr/'.'repittcode='.$repittCode.'.png');
 
         while (User::where('repitt_code', $repittCode)->exists()) {
             $repittCode = $this->generateRepittCode();
         }
         $user->repitt_code = $repittCode;
+        $user->qr_path = asset('storage/business/images/qr/' . 'repittcode=' . $user->repitt_code . '.png');
 
         switch ($request->role) {
             case 'Owner':
@@ -47,9 +48,85 @@ class UserController extends Controller
         return $user;
     }
 
+    public function update(Request $request)
+    {
+        try {
+            $authenticatedUser = auth()->user();
+            // modificar todos los datos que el usuario envio 
+            $user = User::find($authenticatedUser->id);
+            $user->first_name = (isset($request->first_name)) ? $request->first_name : $authenticatedUser->first_name;
+            $user->last_name = (isset($request->last_name)) ? $request->last_name : $authenticatedUser->last_name;
+            $user->phone = (isset($request->phone)) ? $request->phone : $authenticatedUser->phone;
+            $user->email = (isset($request->email)) ? $request->email : $authenticatedUser->email;
+            // si modifico el correo poner el has_verified_email como false
+            if ($user->isDirty('email')) {
+                $user->has_verified_email = 0;
+            }
+            $user->save();
+        } catch (Exception $e) {
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'error' => $e->getMessage()
+                ],403
+                );
+        }
+        return response()->json(
+            [
+                'status' => 'success',
+                'message' => 'user data has changed'
+            ],
+            200
+        );
+    }
+
+    public function updatePassword(Request $request)
+    {
+        $rules = [
+            'activePassword' => 'required|string', // contraseña actual del usuario
+            'newPassword' => 'required|string' // contraseña que el usuario quiere usar
+        ];
+        $validation = validator::make($request->input(),$rules);
+        if($validation->fails())
+        {
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'error' => $validation->errors()->all()
+                ],401
+            );
+        }
+        try {
+            $authenticatedUser = auth()->user();
+            if(!Hash::check($request->activePassword,$authenticatedUser->password))
+            {
+                throw new Exception("Wrong password", 1);
+            }
+            if($request->activePassword == $request->newPassword)
+            {
+                throw new Exception("Your new passsord has to be different to your actual password", 1);
+            }
+            User::where('id',$authenticatedUser->id)->update(['password' => Hash::make($request->newPassword)]);
+            app(EmailController::class)->notifyPasswordChange($authenticatedUser->name,$authenticatedUser->email);
+            return response()->json(
+                [
+                    'status' => 'success',
+                    'message' => 'password changed successfully'
+                ],200
+            );
+        } catch (Exception $e) {
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => $e->getMessage()
+                ],403
+            );
+        }
+    }
+
     public function getCurrentVisitorData()
     {
-        try{
+        try {
             $user = auth()->user();
             return response()->json(
                 [
@@ -57,14 +134,16 @@ class UserController extends Controller
                     'data' => [
                         $user
                     ]
-                ],200
+                ],
+                200
             );
-        }catch(Exception $e){
+        } catch (Exception $e) {
             return $e;
         }
     }
 
-    private function generateRepittCode(){
+    private function generateRepittCode()
+    {
         $alphabet = 'abcdefghijklmnopqrstuvwxyz';
         $randomIndex = rand(0, strlen($alphabet) - 1);
         $randomCharacter = $alphabet[$randomIndex];
@@ -78,20 +157,20 @@ class UserController extends Controller
     private function generateQr($repittCode)
     {
         $qrCode = QrCode::format('png')
-                ->size(200)
-                ->errorCorrection('H')
-                ->generate($repittCode);
+            ->size(200)
+            ->errorCorrection('H')
+            ->generate($repittCode);
 
-        Storage::disk('public')->put('business/images/qr/'.'repittcode='.$repittCode.'.png',$qrCode);
+        Storage::disk('public')->put('business/images/qr/' . 'repittcode=' . $repittCode . '.png', $qrCode);
 
     }
-    // private function saveqrPath($repittCode,$userId)
-    // {
-    //     $this->generateQr($repittCode,$userId);
+// private function saveqrPath($repittCode,$userId)
+// {
+//     $this->generateQr($repittCode,$userId);
 
-    //     $user = User::find($userId);
-    //     $user->qr_path = asset('storage/business/images/qr/'.$userId.'.png');
-    //     $user->save();
+//     $user = User::find($userId);
+//     $user->qr_path = asset('storage/business/images/qr/'.$userId.'.png');
+//     $user->save();
 
-    // }
+// }
 }
