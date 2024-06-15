324b5d858e1964d507b0c7e458a2bb175b4e5c36
xCHZx <chzx94@gmail.com>
2024-06-14 20:41:00 -0500

    Improve paremeters in scheduler

--- routes/console.php
+++ routes/console.php
@@ -10,7 +10,27 @@ use App\Helpers\DataValidation;
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 // desctivar las cards que ya hayan expirado
-Schedule::call(function(){
-    $dataValidation = new DataValidation;
-    $dataValidation->DeactivateStampCards();
-})->everyMinute();
+
+
+//Compare .ENV APP_ENV=local
+
+if (env('APP_ENV') == 'local') {
+    Schedule::call(function(){
+        $dataValidation = new DataValidation;
+        $dataValidation->DeactivateStampCards();
+    })->everyMinute();
+}
+
+if (env('APP_ENV') == 'staging') {
+    Schedule::call(function(){
+        $dataValidation = new DataValidation;
+        $dataValidation->changeBusinessessQrByTinker();
+    })->everyMinute();
+}
+
+if (env('APP_ENV') == 'production') {
+    Schedule::call(function(){
+        $dataValidation = new DataValidation;
+        $dataValidation->changeBusinessFlyerByTinker();
+    })->daily();
+}
